2436fd05cc340b020445f466f30adffd7e39fa13
rgt219 <rgt219@outlook.com>
2026-03-16 12:07:01 -0400

    few changes

--- YuGiOh_Deck_React/src/config.js
+++ YuGiOh_Deck_React/src/config.js
@@ -3,7 +3,7 @@ const isLocal = window.location.hostname === "localhost";
 export const API_URLS = {
     IDENTITY: isLocal 
         ? "https://localhost:5001/api/users" 
-        : "https://api.happybush-e43d89b2.eastus.azurecontainerapps.io/api/Users",
+        : "https://identity.happybush-e43d89b2.eastus.azurecontainerapps.io/api/Users",
     DECK: isLocal 
         ? "https://localhost:5002/api/decks" 
         : "https://api.happybush-e43d89b2.eastus.azurecontainerapps.io/api/mongodb/DeckListMongoDb"
