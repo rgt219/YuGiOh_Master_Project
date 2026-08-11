6a4e946dcf372372d0d21ef26428bb2840c07277
rgt219 <rgt219@outlook.com>
2026-08-10 20:03:30 -0400

    fixed config file

--- YuGiOh_Deck_React/next.config.js
+++ YuGiOh_Deck_React/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // ❌ Remove this line if you want standard server-side/dynamic rendering
+  output: 'standalone', // ❌ Remove this line if you want standard server-side/dynamic rendering
   images: {
     unoptimized: true,
   },
