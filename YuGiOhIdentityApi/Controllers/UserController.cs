6c999db83541a62801705bea0dcf0f4ba07b5717
rgt219 <rgt219@outlook.com>
2026-08-19 09:53:56 -0400

    Forgot comma

--- YuGiOhIdentityApi/Controllers/UserController.cs
+++ YuGiOhIdentityApi/Controllers/UserController.cs
@@ -82,8 +82,9 @@ public class UsersController : ControllerBase
         var claims = new[] {
             new Claim(JwtRegisteredClaimNames.Sub, user.UserName ?? ""),
             new Claim(JwtRegisteredClaimNames.Email, user.Email ?? ""),
+            new Claim(ClaimTypes.NameIdentifier, user.Id.ToString() ?? ""),
             new Claim("userId", user.Id.ToString() ?? ""),
-            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
             new Claim(ClaimTypes.Name, user.UserName ?? "")
         };
 
